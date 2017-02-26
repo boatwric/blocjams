76250dbce448c41f9b30315cde71d0e8e933fe9a
Christopher Boatwright <chris@Roke.local>
2017-02-26 17:38:16 -0500

    Checkpoint 9 Commit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,34 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');/*defines what's being manipulated and saves them to points; in this case it looks at the document (HTML), uses a selector to return all the nodes as a list, and specifies in the parathesis to gather up all the elements with an ID of point*/
- 
-     var revealFirstPoint = function() { /*three more functions nested in animatePoints function*/
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }; /*goes through all animation calculations, one nested function at a time*/ 
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
- }; /*closes off whole function, end with semicolon since it was assigned to a variable*/
-animatePoints();
+var pointsArray = document.getElementsByClassName('point');/*defines what's being manipulated and saves them to points; in this case it looks at the document (HTML), uses a selector to return all the nodes as a list, and specifies in the parathesis to gather up all the elements with an ID of point*/
+  
+
+var animatePoints = function(points) { //Had a devil of a time getting this to load once 'points' became 'pointsArray' and 'animatePoints got 'points' passed in as a parameter//
+  
+  var revealPoint = function() {
+
+     for (var i = 0; i < points.length; i++) {
+       points[i].style.opacity = 1;
+       points[i].style.transform = "scaleX(1) translateY(0)";
+       points[i].style.msTransform = "scaleX(1) translateY(0)";
+       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+     }
+  };
+    revealPoint();
+}; /*closes off whole function, end with semicolon since it was assigned to a variable*/
+
+//Used to be a function call here that was messing up everything below. LESSON- DON'T ALWAYS CALL YOUR FUNCTION RIGHT AWAY!//
+
+window.onload = function() {
+     
+  if (window.innerHeight > 950) { //looks for window height before anything; if it didn't, larger screens would never get the selling points at all//
+         animatePoints(pointsArray);
+     }
+  
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; //this sets the point at which animations should load//
+
+     window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { //basically says if it passes this scroll distance, fire the 'animatePoints' function//
+             animatePoints(pointsArray);   
+         }     
+     });
+ }
