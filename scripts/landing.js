ba658f84198dc5d71efb6147b846fb1510f8d1d7
Christopher Boatwright <chris@roke.gateway.pace.com>
2017-02-26 00:38:01 -0500

    Checkpoint 8 Commit FINAL

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,10 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');/*defines what's being manipulated and saves them to points; in this case it looks at the document (HTML), uses a selector to return all the nodes as a list, and specifies in the parathesis to gather up all the elements with an ID of point*/
- 
-     var revealFirstPoint = function() { /*three more functions nested in animatePoints function*/
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }; /*goes through all animation calculations, one nested function at a time*/ 
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
- }; /*closes off whole function, end with semicolon since it was assigned to a variable*/
-animatePoints();
+var revealPoint = function() {
+ var points = document.getElementsByClassName('point');
+   for (var i = 0; i < points.length; i++) {
+     points[i].style.opacity = 1;
+     points[i].style.transform = "scaleX(1) translateY(0)";
+     points[i].style.msTransform = "scaleX(1) translateY(0)";
+     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+ }
+};
+revealPoint();
